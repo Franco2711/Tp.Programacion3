40e8fd11ec877b9df6f70290589f83c547ba1a78
Franco2711 <franconuttini@hotmail.com.ar>
2026-08-30 20:35:28 -0300

    Numeros aleatoreos
    Ya salen numeros random desde el lugar opuesto al movimiento

--- View/PracticaVentanas.java
+++ View/PracticaVentanas.java
@@ -1,5 +1,7 @@
 package View;
 
+import java.util.Random;
+
 public class PracticaVentanas {
 	
 	static int [][] m = {{0,0,0,0}, 
@@ -18,7 +20,11 @@ public class PracticaVentanas {
 			}
 		}for(int c = 0; c < m[0].length; c++) {
 			m[3][c] = 0;
-		}
+		}int valor = nuevoNumeroAInsertar();
+		int posicion = nuevaPosicionAIntertar();
+		while(m[3][posicion] != 0) {
+			posicion = nuevaPosicionAIntertar();
+		}m[3][posicion] = valor;
 	}
 	
 	public static void moverPorFilasAbajo(int[][] m) {
@@ -28,7 +34,11 @@ public class PracticaVentanas {
 			}
 		}for(int c = 0; c < m[0].length; c++) {
 			m[0][c] = 0;
-		}
+		}int valor = nuevoNumeroAInsertar();
+		int posicion = nuevaPosicionAIntertar();
+		while(m[0][posicion] != 0) {
+			posicion = nuevaPosicionAIntertar();
+		}m[0][posicion] = valor;
 	}
 	
 	public static void moverPorColumnasDerecha(int[][] m) {
@@ -38,7 +48,11 @@ public class PracticaVentanas {
 			}
 		}for(int f = 0; f < m.length; f++) {
 			m[f][0] = 0;
-		}
+		}int valor = nuevoNumeroAInsertar();
+		int posicion = nuevaPosicionAIntertar();
+		while(m[posicion][0] != 0) {
+			posicion = nuevaPosicionAIntertar();
+		}m[posicion][0] = valor;
 	}
 	
 	public static void moverPorColumnasIzquierda(int[][] m) {
@@ -48,15 +62,48 @@ public class PracticaVentanas {
 			}
 		}for(int f = 0; f < m.length; f++) {
 			m[f][3] = 0;
-		}
+		}int valor = nuevoNumeroAInsertar();
+		int posicion = nuevaPosicionAIntertar();
+		while(m[posicion][3] != 0) {
+			posicion = nuevaPosicionAIntertar();
+		}m[posicion][3] = valor;
 	}
+	
 	public static void imprimirMatriz(int[][] m) {
 		for(int f = 0; f < m.length; f++) {
 			System.out.print("[ ");
 			for(int c = 0; c < m[0].length; c++ ) {
 				System.out.print(m[f][c] + " ");
-		}System.out.println("]");
-	}System.out.println();
+			}System.out.println("]");
+		}System.out.println();
+	}
+	
+	public static void insertarEnMovimientoDerecha(int[][] matriz, int posicion, int valor) {
+		matriz[posicion][0] = valor;
+	}
+	
+	public static void insertarEnMovimientoIzquierda(int[][] matriz, int posicion, int valor) {
+		matriz[posicion][3] = valor;
+	}
+	
+	public static void insertarEnMovimientoArriba(int[][] matriz, int posicion, int valor) {
+		matriz[3][posicion] = valor;
+	}
+	
+	public static void insertarEnMovimientoAbajo(int[][] matriz, int posicion, int valor) {
+		matriz[0][posicion] = valor;
+	}
+	
+	public static int nuevoNumeroAInsertar() {
+		Random rand = new Random();
+		int numeroNuevo = rand.nextInt(3) + 1;
+		return numeroNuevo;
+	}
+	
+	public static int nuevaPosicionAIntertar() {
+		Random rand = new Random();
+		int numeroNuevo = rand.nextInt(4);
+		return numeroNuevo;
 	}
 		
 public static void main(String[] args) {
